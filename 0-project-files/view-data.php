2c0483bfe0e196a362095ca88894bdf2db4c6956
GunaRakulan-GR <gunarakulan@gmail.com>
2022-02-17 17:31:34 +0530

    Add function to open website

--- 0-project-files/view-data.php
+++ 0-project-files/view-data.php
@@ -29,24 +29,26 @@
       <th scope="col">Website Name</th>
       <th scope="col">URL</th>
       <th scope="col">Description</th>
+      <th scope="col">View Site</th>
       <th scope="col">Actions</th>
     </tr>
   </thead>
   <tbody>
-    
+
 
     <?php
-    
+
         if ($result->num_rows > 0) {
 
             $counter = 1;
-            
+
             while($row = $result->fetch_assoc()) {
                 echo "<tr>";
                     echo "<td>$counter</td>";
                     echo '<td>'.$row["website_name"].'</td>';
                     echo '<td>'.$row["website_url"].'</td>';
                     echo '<td>'.$row["description"].'</td>';
+                    echo "<td><a href='".$row["website_url"]."' target='_blank' class='btn btn-success'>Open</a></td>";
                     echo "<td><a href='delete-data.php?id=".$row["auto_id"]."' class='btn btn-danger'>Delete</a></td>";
                 echo "</tr>";
 
@@ -57,7 +59,7 @@
 
     ?>
 
-    
+
   </tbody>
 </table>
 
